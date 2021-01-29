574a6a3ad40851fc7444d5d21745faac7d539f7f
Luis Rosario <luisalmonter@gmail.com>
2021-01-29 17:00:55 -0500

    adds second project pictures and slide

--- js/script.js
+++ js/script.js
@@ -25,3 +25,31 @@ function showSlides(n) {
     slides[slideIndex-1].style.display = 'block';
     dots[slideIndex-1].className += 'active';
 }
+
+let slideIndex1 = 1;
+showSlides1(slideIndex1);
+
+function plusSlides1(x) {
+    showSlides1(slideIndex1 += x);
+}
+
+function currentSlide1(x) {
+    showSlides1(slideIndex1 = x);
+}
+
+function showSlides1(x) {
+    let a;
+    let slides1 = document.getElementsByClassName('post');
+    let dots1 = document.getElementsByClassName('dot1');
+    if (x > slides1.length) {slideIndex1 = 1}
+    if (x < 1) {slideIndex1 = slides1.length}
+    for (a = 0; a < slides1.length; a++) {
+        slides1[a].style.display = 'none';
+    }
+    for (a = 0; a < dots1.length; a++) {
+        dots1[a].className = dots1[a].className.replace('active', '');
+    }
+    
+    slides1[slideIndex1-1].style.display = 'block';
+    dots1[slideIndex1-1].className += 'active';
+}
